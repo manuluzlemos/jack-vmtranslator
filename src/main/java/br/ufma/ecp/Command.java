6005753c89e9fb700cd5e7d6123deea42bcdc872
Emanuelle Lemos <manuluz.lemos@gmail.com>
2022-07-17 17:50:30 -0300

    primeira parte do tradutor

---
+++ src/main/java/br/ufma/ecp/Command.java
@@ -0,0 +1,39 @@
+package br.ufma.ecp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Command {
+
+    public enum Type {
+        ADD, 
+        SUB,
+        NEG,
+        EQ,
+        GT,
+        LT,
+        AND,
+        OR,
+        NOT,
+        PUSH,
+        POP
+        
+        ;
+    }
+
+    public Command.Type type;
+    public List<String> args = new ArrayList<>();
+
+    public Command (String[] command) {
+
+        type = Command.Type.valueOf(command[0].toUpperCase());
+
+        for (int i=1;i<command.length;i++){
+            var arg = command[i];
+            var pos = arg.indexOf("//");
+            if (pos != -1) arg = arg.substring(0, pos);
+            args.add(arg.strip());
+        } 
+    }
+    
+}

aa729da5be7e738853ed05282d331f38f3c31b62
Emanuelle Lemos <manuluz.lemos@gmail.com>
2022-07-18 16:32:45 -0300

    deletando importação de bibliotecas não utilizadas

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -3,11 +3,6 @@ package br.ufma.ecp;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
 
 public class App {
 
